217572c7dbfdb6c6eaaa8990a68f0f2963135d38
gamma_02 <gammagames06@gmail.com>
2023-10-19 19:39:41 -0700

    server resource pack reload before item registry freezing, new socks texture

---
+++ src/main/java/net/gamma02/thighhigh/mixin/FreezeRegistryMixin.java
@@ -0,0 +1,41 @@
+package net.gamma02.thighhigh.mixin;
+
+
+import net.minecraft.client.gui.screen.world.CreateWorldScreen;
+import net.minecraft.item.Item;
+import net.minecraft.registry.*;
+import net.minecraft.resource.DataConfiguration;
+import net.minecraft.resource.ResourcePackManager;
+import net.minecraft.resource.VanillaDataPackProvider;
+import net.minecraft.resource.featuretoggle.FeatureFlags;
+import net.minecraft.server.SaveLoading;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Objects;
+
+import static net.minecraft.server.SaveLoading.*;
+
+@Mixin(SimpleRegistry.class)
+public class FreezeRegistryMixin<T> {
+
+    @Shadow @Final private RegistryKey<? extends Registry<T>> key;
+
+    @Inject(method = "freeze", at = @At("HEAD"))
+    public void freezeMixin(CallbackInfoReturnable<Registry<T>> cir){
+//        if(cir.getReturnValue().em)
+        if(Objects.equals(this.key, RegistryKeys.ITEM)) {
+            System.out.println("Reloading before");
+            ResourcePackManager resourcePackManager = new ResourcePackManager(new VanillaDataPackProvider());
+//        var dataConfiguration = DataConfiguration.SAFE_MODE.withFeaturesAdded(this.initialDataConfig.enabledFeatures()); - find a way to do this with all of them if this doesn't work!
+            SaveLoading.DataPacks dataPacks = new SaveLoading.DataPacks(resourcePackManager, DataConfiguration.SAFE_MODE.withFeaturesAdded(FeatureFlags.DEFAULT_ENABLED_FEATURES), false, true);
+
+            dataPacks.load();//whops gotta have that lmaa
+        }
+
+    }
+}
